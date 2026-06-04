c3e9781ba1049dd19655a4a11a3c92e5e9b068d9
naitikmundra <naitikmundra18@gmail.com>
2026-06-04 10:36:35 +0530

    Implement getAllBlogPosts web method
    Backend File

---
+++ exportblog.web.js
@@ -0,0 +1,57 @@
+// backend/getAllBlogPosts.web.js
+// An Exporter by Naitik Mundra
+import { Permissions, webMethod } from "wix-web-module";
+import { posts } from "wix-blog-backend";
+
+// fetch full text content of a single post
+export async function getPostContent(postId) {
+    const options = {
+        fieldsets:  (["RICH_CONTENT"])
+    };
+
+    try {
+        const result = await posts.getPost(postId, options);
+        return result.post;
+    } catch (error) {
+        console.error("getPostContent error:", error);
+        return null;
+    }
+}
+
+export const getAllBlogPosts = webMethod(
+    Permissions.Anyone,
+    async () => {
+        try {
+            const result = await posts.queryPosts()
+                .limit(2)
+                .find();
+
+            const formatted = await Promise.all(
+                result.items.map(async (post) => {
+
+                    let content = await getPostContent(post._id);
+
+                    if (!content) {
+                        content = "Failed to Fetch";
+                    }
+
+                    return {
+                        id: post._id,
+                        title: post.title,
+                        slug: post.slug,
+                        excerpt: post.excerpt,
+                        publishedDate: post.firstPublishedDate,
+                        category: post.categoryIds,
+                        html: content
+                    };
+                })
+            );
+
+            return formatted;
+
+        } catch (e) {
+            console.error("getAllBlogPosts error:", e);
+            return [];
+        }
+    }
+);
